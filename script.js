1927e3c1b3c8e8228c5176c2fabb6955a3638072
maskevaidehi8-beep <maskevaidehi8@gmail.com>
2026-02-17 16:03:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if(name === "" || email === "" || message === "") {
+        document.getElementById("formMessage").innerText = "Please fill all fields!";
+    } else {
+        document.getElementById("formMessage").innerText = "Message sent successfully!";
+        this.reset();
+    }
+});
